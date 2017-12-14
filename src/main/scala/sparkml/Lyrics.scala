4125b4c48eaab61e4913e3209b3ad99616c44e88
Mary Yang <jyang@trinity.edu>
2017-12-14 05:18:19 -0600

    final project

--- src/main/scala/sparkml/Lyrics.scala
+++ src/main/scala/sparkml/Lyrics.scala
@@ -6,6 +6,12 @@ import org.apache.spark.sql.Row
 import org.apache.spark.ml.clustering.KMeans
 import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.ml.feature.Normalizer
+import org.apache.spark.ml.feature.StringIndexer
+import org.apache.spark.ml.evaluation._
+import org.apache.spark.mllib.stat.correlation
+import org.apache.spark.ml.stat.Correlation
+import org.apache.spark.ml.linalg.{Matrix, Vectors}
+import org.apache.spark.ml.classification._
 import scalafx.application.JFXApp
 import swiftvis2.plotting._
 import swiftvis2.plotting.renderer.FXRenderer
@@ -48,10 +54,27 @@ implicit class OpsNum(val str: String) extends AnyVal {
   }}
 
   val modeUDF = udf{s: Seq[String] => {
-      s.groupBy(i=>i).mapValues(_.size).toSeq.sortBy(_._2 * (-1)).slice(0, 5) 
+      s.groupBy(i=>i).mapValues(_.size).toSeq.maxBy(_._2)
+      //s.groupBy(i=>i).mapValues(_.size).toSeq.filter(_._2 != 1).slice(0, 3)
   }}
 
-  val spark = SparkSession.builder.appName("Lyrics").master("local[*]").getOrCreate()
+
+  val countUDF = udf{s:Seq[String] => {
+    s.size 
+  
+  }}
+  
+  val wordUDF = udf{(s:Seq[String], word: String)=>{
+    s.count(_ == word)    
+  }}
+
+  val allGenres = "Metal Pop Hip-Hop R&B Folk Rock Indie Electronic Jazz Country".split(" ")
+  val indexerUDF = udf{s: String => {
+    allGenres.indexOf(s)   
+  }}
+
+  //val spark = SparkSession.builder.appName("Lyrics").master("local[*]").getOrCreate()
+  val spark = SparkSession.builder.appName("Lyrics").getOrCreate()
   import spark.implicits._
   val sc = spark.sparkContext
 
@@ -89,8 +112,6 @@ implicit class OpsNum(val str: String) extends AnyVal {
         sc.parallelize(songs.slice(0, counter))
   }
   val colFile = getColFile("/data/BigData/students/jyang/lyrics.csv") 
-  
-  println("count: " + colFile.count())
  
   val mainData = spark.createDataFrame(colFile, schema)
 
@@ -105,7 +126,41 @@ implicit class OpsNum(val str: String) extends AnyVal {
   //pronouns.groupBy('genre).agg(sum("len"), count("len"), avg("len")).show()
   
   //common words
-  mainData.groupBy('genre).agg(flatten(collect_list("lyrics")) as "all_words").withColumn("mode", modeUDF('all_words)).show()
+  //mainData.groupBy('genre).agg(flatten(collect_list("lyrics")) as "all_words").withColumn("mode", modeUDF('all_words)).show()
+ /*
+ val downSized = mainData.limit(10000).withColumn("word", explode('lyrics)).select('genre, 'word, 'song)
+  val mundaneWords = Set("love", "dem", "que","are", "no", "from", "be", "it's", "will", "i'm", "is", "all","me", "my", "it", "they", "them", "their","your", "we", "us", "our", "the", "of", "a", "this", "that", "to", "and", "in", "have", "i", "for", "not", "on", "with", "you").toSeq
+  val wordsNcounts = downSized.groupBy('genre, 'word).count().filter(!'word.isin(mundaneWords:_*))
+  wordsNcounts.show()
+  val maxCounts = wordsNcounts.groupBy('genre).agg(max("count") as "count")
+  maxCounts.join(wordsNcounts, maxCounts("genre") === wordsNcounts("genre") && maxCounts("count") === wordsNcounts("count")).show()
+  
+*/
+ val idWords = List("is", "all", "love", "it's", "jazz", "her", "baby", "so", "dem", "i'm", "when", "que", "like", "get")
+ val songWords = idWords.foldLeft(mainData)((df, word) => df.withColumn(word, wordUDF('lyrics, lit(word)))).filter('genre =!= "Not Available" && 'genre =!= "Other").limit(1000)
+ //songWords.groupBy('genre).agg(sum("is") as "is",sum("all") as "all",sum("love") as "love",sum("it's") as "it's",sum("jazz") as "jazz",sum("her") as "her",sum("baby") as "baby",sum("so") as "so",sum("dem") as "dem",sum("i'm") as "i'm",sum("when") as "when",sum("que") as "que",sum("like") as "like",sum("get") as "get").show()
+  val inputVa = new VectorAssembler().setInputCols(idWords.toArray).setOutputCol("features")
+  //val indexer = new StringIndexer().setInputCol("genre").setOutputCol("label")
+  //val df = indexer.fit(songWords).transform(songWords)
+  val inputAssembled = inputVa.transform(songWords).select('genre, 'features)
+  println("va")
+  inputAssembled.show()
+  val df = inputAssembled.withColumn("label", indexerUDF('genre))
+  println("df")
+  df.show()
+  val df2 = df.select('label as "old", 'features).withColumn("label", when('old === 0, 0).otherwise( 1))
+  val Array(train, test) = df2.randomSplit(Array(0.8, 0.2)).map(_.cache())
+
+  val rf = new RandomForestClassifier()
+  val gbt = new GBTClassifier
+  val model = gbt.fit(train)
+  println("model trained")
+  val predictions = model.transform(test)
+  predictions.show()
+  val evaluator = new MulticlassClassificationEvaluator
+  val bevaluator = new BinaryClassificationEvaluator
+  val accuracy = bevaluator.evaluate(predictions)
+  println(s"accuracy = $accuracy")
   
   spark.stop()
 
